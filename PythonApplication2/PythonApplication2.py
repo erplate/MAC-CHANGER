4d613796c2bf01418d36ce1ed97aaeef48e27900
erplate <88729931+erplate@users.noreply.github.com>
2021-10-10 17:31:24 +0300

    Update PythonApplication2.py
    Program has been rewritten.

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -6,11 +6,10 @@ def change_mac(interface,new_mac):
     subprocess.call(["ifcoming",interface,"down"])
     subprocess.call(["ifcoming",interface,"hw","ether",new_mac])
     subprocess.call(["ifcoming",interface,"up"])
-
-parser=optparse.OptionParser()
-parser.add_option("-i","--interface",dest="interface",help="Interface to change its Mac-adress")
-parser.add_option("-m","--mac",dest="new mac",help="New Mac adress")
-
-(options,arguments)=parser.parse_args()
-
+def get_arguments():
+    parser=optparse.OptionParser()
+    parser.add_option("-i","--interface",dest="interface",help="Interface to change its Mac-adress")
+    parser.add_option("-m","--mac",dest="new mac",help="New Mac adress")
+    return parser.parse_args()
+(options,arguments)=get_argumentss()
 change_mac(options.interface,options.new_mac)
